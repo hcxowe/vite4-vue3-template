661bc3bcbfc599fb270054a7ba7ee1b7248ef918
hcxowe <hcxowe@126.com>
2023-08-31 20:54:39 +0800

    模板语法；响应式对象；

--- src/views/vue/base/Base.vue
+++ src/views/vue/base/Base.vue
@@ -1,3 +1,38 @@
 <template>
-    vue-base
+    <div :data-index="dataIndex" @click.native="onMsgClick">{{ showMsg }}</div>
+
+    <div @click="reactiveObj.data.name = 'yx'">reactive深层次响应：{{ reactiveObj.data.name }}</div>
+
+    <div @click="shallowReactiveObj.data.name = 'yx'">shallowReactive浅层次响应：{{ shallowReactiveObj.data.name }}</div>
 </template>
+
+<script lang="ts" setup>
+import { ref, reactive, shallowReactive, nextTick } from 'vue'
+
+const dataIndex = ref(0)
+const showMsg = ref('this is template string')
+
+const reactiveObj = reactive({
+    count: 0,
+    data: {
+        name: 'cx',
+        age: '30'
+    }
+})
+
+const shallowReactiveObj = shallowReactive({
+    count: 0,
+    data: {
+        name: 'shallowName',
+        age: '30'
+    }
+})
+
+const onMsgClick = () => {
+    showMsg.value = 'msg is clicked, count: ' + ++dataIndex.value
+}
+
+nextTick(() => {
+    console.log('base component is updated!')
+})
+</script>
