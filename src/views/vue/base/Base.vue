eac5646f7c896c90789de05a4c6a5f7281ad87b2
hcxowe <hcxowe@126.com>
2023-09-01 17:34:03 +0800

    comuted, watch

--- src/views/vue/base/Base.vue
+++ src/views/vue/base/Base.vue
@@ -1,13 +1,25 @@
 <template>
     <div :data-index="dataIndex" @click.native="onMsgClick">{{ showMsg }}</div>
     <br>
-    <div @click="reactiveObj.data.name = 'yx'">reactive深层次响应：{{ reactiveObj.data.name }}</div>
+    <div @click="reactiveObj.data.name = 'yx'" style="border-top: 1px solid #ccc;">reactive深层次响应：{{ reactiveObj.data.name }}</div>
     <br>
     <div @click="shallowReactiveObj.data.name = 'yx'">shallowReactive浅层次响应：{{ shallowReactiveObj.data.name }}</div>
+    <br>
+    <p style="border-top: 1px solid #ccc;">计算属性：</p>
+    <el-input v-model="textValue" />
+    <p>{{ computeTextValue }}</p>
+    <p>{{ computeTextValue1 }}</p>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, shallowReactive, nextTick } from 'vue'
+import { 
+    ref, reactive, shallowReactive, 
+    computed,
+    watch, watchEffect,
+    onMounted, onUnmounted, nextTick 
+} from 'vue'
+
+import { login } from '../../../api/userApi.ts'
 
 const dataIndex = ref(0)
 const showMsg = ref('this is template string')
@@ -34,5 +46,43 @@ const onMsgClick = () => {
 
 nextTick(() => {
     console.log('base component is updated!')
+})
+
+const textValue = ref('')
+
+const computeTextValue = computed(() => {
+    return textValue.value.toUpperCase()
+})
+
+const computeTextValue1 = computed({
+    get() {
+        return textValue.value.toUpperCase() + ' - ' + textValue.value.toLowerCase()
+    },
+    set(value) {
+        textValue.value = value
+    }
+})
+
+onMounted(() => {
+    console.log('mounted')
+})
+
+watch(textValue, (newValue, oldValue) => {
+    console.log(newValue, oldValue)
+})
+
+// 监听响应式对象的属性值
+const obj = reactive({ count: 0 })
+watch(() => obj.count, (count) => {
+    console.log(count)
+}, { 
+    deep: true,     // 深层侦听
+    immediate: true // 建侦听器时，立即执行一遍回调
+})
+
+const username = ref('')
+// watchEffect自动跟踪回调的响应式依赖，username改变自动发起请求
+watchEffect(async () => {
+    const response = await login(username.value, 'password')
 })
 </script>
